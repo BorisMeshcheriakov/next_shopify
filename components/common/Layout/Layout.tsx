efce3662a67b2afe7b770444511c1c299c7ce60b
meshcheriakov.b@nicecode.biz <meshcheriakov.b@nicecode.biz>
2022-02-18 17:47:04 +0300

    add(style): setup tailwindcss and postcss

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Layout: React.FC = ({ children }) => {
-	return <div className="layout">{children}</div>;
+	return <main className="root">{children}</main>;
 };
 
 export default Layout;

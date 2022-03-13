0e03425859909d82f3c2594bd51ef74f8cb91047
BorisMeshcheriakov <incarnate6@gmail.com>
2022-03-14 02:04:04 +0300

    add(cart): useSWR hook

---
+++ framework/shopify/cart/use-cart.ts
@@ -0,0 +1,21 @@
+import useCart from '@common/cart/use-cart';
+
+export default useCart;
+
+export const handler = {
+	fetchOptions: {
+		query: 'query { hello }',
+	},
+	async fetcher({ fetch, options, input: { checkoutId } }: any) {
+		const data = await fetch({ ...options });
+
+		return { data };
+	},
+	useHook: ({ useData }: any) => {
+		const data = useData();
+
+		return {
+			data,
+		};
+	},
+};

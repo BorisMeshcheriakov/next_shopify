44e4cf172369bd0276c0b3fb32546bec2cf398fe
BorisMeshcheriakov <incarnate6@gmail.com>
2022-03-20 19:07:17 +0300

    add(cart): cart normalization

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -5,8 +5,64 @@ import {
 	ProductOption,
 	ProductVariantConnection,
 	SelectedOption,
+	Checkout,
+	CheckoutLineItemEdge,
 } from '../schema';
 import { Product } from '@common/types/product';
+import { Cart, LineItem } from '@common/types/cart';
+
+export const normalizeCart = (checkout: Checkout): Cart => {
+	return {
+		id: checkout.id,
+		createdAt: checkout.createdAt,
+		currency: {
+			code: checkout.totalPriceV2.currencyCode,
+		},
+		taxesIncluded: checkout.taxesIncluded,
+		lineItemsSubtotalPrice: +checkout.subtotalPriceV2.amount,
+		totalPrice: checkout.totalPriceV2.amount,
+		lineItems: checkout.lineItems.edges.map((lineItemEdge) => lineItemEdge.node),
+		discounts: [],
+	};
+};
+
+const normalizeLineItem = ({
+	node: { id, title, variant, quantity, ...rest },
+}: CheckoutLineItemEdge): LineItem => {
+	return {
+		id,
+		variantId: String(variant?.id),
+		productId: String(variant?.id),
+		name: title,
+		quantity,
+		path: String(variant?.product?.handle),
+		discounts: [],
+		options: variant?.selectedOptions.map(({ name, value }: SelectedOption) => {
+			const option = normalizeProductOption({
+				id,
+				name,
+				values: [value],
+			});
+
+			return option;
+		}),
+		variant: {
+			id: String(variant?.id),
+			sku: variant?.sku ?? '',
+			name: variant?.title,
+			image: {
+				url:
+					process.env.NEXT_PUBLIC_FRAMEWORK === 'shopify_local'
+						? `/images/${variant?.image?.originalSrc}`
+						: variant?.image?.originalSrc ?? '/product-image-placeholder.svg',
+			},
+			requiresShipping: variant.requiresShipping ?? false,
+			price: variant?.priceV2.amount,
+			listPrice: variant?.compareAtPriceV2.amount,
+		},
+		...rest,
+	};
+};
 
 const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
 	edges.map(({ node: { originalSrc: url, ...rest } }) => ({
